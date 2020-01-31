f405d16f5b8a2e0f9e0943319f579d238a3ebb6d
Cloud-42 <hello@cloud42.io>
2020-01-31 13:48:15 +0000

    Adding Vars for trust identifers

--- data.tf
+++ data.tf
@@ -6,8 +6,8 @@ data "aws_iam_policy_document" "this" {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+      type        = var.principal_type
+      identifiers = var.principal_identifiers
     }
   }
 }
