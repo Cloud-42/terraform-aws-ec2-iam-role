55b8b2d498f1152c084c20010bbb7188ab7663c1
Cloud-42 <chris@cloud42.io>
2021-02-12 08:57:44 +0000

    Add Name or name prefix

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@
 # Role, Profile and attachment
 # ------------------
 resource "aws_iam_role" "this" {
-  name_prefix           = substr(var.name, 0, 22)
+  name                  = var.name != null ? var.name : null
+  name_prefix           = var.name_prefix != null ? substr(var.name_prefix, 0, 22) : null
   assume_role_policy    = var.assume_role_policy == "" ? data.aws_iam_policy_document.this.json : var.assume_role_policy
   force_detach_policies = var.force_detach_policies
   path                  = var.path
